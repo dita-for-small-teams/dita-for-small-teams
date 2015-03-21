05f027d8153a1f9fa750b55cc4fa7c824be1d324
Eliot Kimber <drmacro@gmail.com>
2015-03-21 12:46:36 -0500

    Batch file minially works

--- basex/scripts/install-modules.bat
+++ basex/scripts/install-modules.bat
@@ -6,6 +6,6 @@ REM path (see BaseX documentation)
 
 set BAT_HOME=%~dp0
 @echo Installing DFST XQuery packages:
-basexclient -U %1 -P %2 -c "repo install %BAT_HOME\..\..\modules\util\dita-utils.xqm"
-basexclient -U %1 -P %2 -c "repo install %BAT_HOME\..\..\modules\util\relpath-utils.xqm"
-basexclient -U %1 -P %2 -c "repo list"
+call basexclient -U %1 -P %2 -c "repo install %BAT_HOME%\..\..\modules\util\dita-utils.xqm"
+call basexclient -U %1 -P %2 -c "repo install %BAT_HOME%\..\..\modules\util\relpath-utils.xqm"
+call basexclient -U %1 -P %2 -c "repo list"
